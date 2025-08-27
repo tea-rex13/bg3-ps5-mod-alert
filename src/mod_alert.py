ce51e300983c6ebd090d7b672412cebc77cebfaf
Kate Baker-Bassett <bakerbassett@gmail.com>
2025-08-27 09:12:50 +0100

    Scaffold get_mod_count() (API) – placeholder

---
+++ src/mod_alert.py
@@ -0,0 +1,8 @@
+
+#placeholder for function will return an error
+def get_mod_count() -> int:
+    """
+    Return the integer count of BG3 PS5 mods.
+    TODO: Implement via API or scraping.
+    """
+    raise NotImplementedError
